7f0fb94bb4f7c1c700dc5a8839e81b46e5ecfa31
abbas99917 <abbasgraphic17@gmail.com>
2026-08-28 03:20:37 -0700

    Events

---
+++ Sections_01-Events/main.js
@@ -0,0 +1,7 @@
+// document.getElementById("btn").onclick = () =>{
+// alert("welcom to js course part two")
+// }
+
+document.getElementById("btn").onclick = function(){
+    alert("this is traditional technique")
+}
