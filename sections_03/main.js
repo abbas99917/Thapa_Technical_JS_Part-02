195e00390d0a2e40a95a9e003be45a751532ef62
abbas99917 <abbasgraphic17@gmail.com>
2026-09-04 03:14:04 -0700

    setTimeOut,setInterval,ClearInterval,

--- sections_03/main.js
+++ sections_03/main.js
@@ -1,5 +1,37 @@
+
+// localStorage
 // current date and time
 // new Date(): create a date object representing the current date and time.
 
-const newDate = new Date()
-console.log(newDate)
+// const newDate = new Date()
+// console.log(newDate)
+
+// setTimeout()
+// setInterval()
+// clearInterval()
+
+
+// setTimeOut
+// setTimeout(()=>{
+//     console.log("this is my car")
+// },5000)
+
+// setInterval()
+
+// let clearInterVal = setInterval(()=>{
+//     console.log("hello world")
+// },2000)
+// clearInterval(clearInterVal)
+
+
+
+const curTime = () =>{
+    let time = new Date().toLocaleTimeString();
+    console.log(time)
+    document.querySelector(".clock").innerHTML = time;
+  
+}
+setInterval(() => {
+    curTime()
+    
+}, 1000);
